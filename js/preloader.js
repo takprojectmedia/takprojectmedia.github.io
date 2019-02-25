6a6c398678e1680aa2703aa1de3b8fd8e85d3da1
FilippAkbas <fakbash@mail.ru>
2019-02-25 21:43:49 +0300

    add preloader

---
+++ js/preloader.js
@@ -0,0 +1,13 @@
+document.body.onload = function() {
+
+
+	setTimeout(function() {
+		var preloader = document.querySelector('.preloader');
+		if( !preloader.classList.contains('done') )
+		{
+			preloader.classList.add('done');
+		}
+	}, 1000);
+
+
+}
